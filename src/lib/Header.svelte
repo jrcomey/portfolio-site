e535eed0775b510c2c00424791deb61981b91ed5
Jack Comey <jack.rhys.comey@gmail.com>
2023-08-14 14:18:23 -0700

    Initial Site Commit

---
+++ src/lib/Header.svelte
@@ -0,0 +1,35 @@
+<script>
+    export let title = "JRC Portfolio Website"
+</script>
+
+<header class="bg-blue-500 text-white p-4">
+    <!-- <h1 class="text-3xl font-semibold">{title}</h1> -->
+    <!-- <nav class="mt-4 flex">
+        <ul class="flex space-x-4">
+            <li><a href="#" class="hover:text-blue-700">Home</a></li>
+            <li><a href="#" class="hover:text-blue-700">Blog</a></li>
+            <li><a href="#" class="hover:text-blue-700">About</a></li>            
+            <li><a href="#" class="hover:text-blue-700">Hire Me</a></li>
+        </ul>
+    </nav> -->
+
+    <!-- <h4>Portfolio | Jack Comey</h4> -->
+            <nav>
+                <ul>
+                  <li><a href="#about-me">About</a></li>
+                  <li><a href="#projects">Projects</a></li>
+                  <li class="dropdown">
+                    <a href="#" class="dropdown-toggle">Blog</a>
+                    <ul class="dropdown-list">
+                      <li><a href="#">Category 1</a></li>
+                      <li><a href="#">Category 2</a></li>
+                      <!-- Add more dropdown menu items as needed -->
+                    </ul>
+                  </li>
+                  <li><a href="https://github.com/jrcomey">GitHub</a></li>
+                  <li><a href="#contact">Contact</a></li>
+                </ul>
+              </nav>
+
+</header>
+
