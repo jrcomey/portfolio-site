f9ca898f1ad73b85022e5ce77a889b42f04c0143
Jack Comey <jack.rhys.comey@gmail.com>
2023-08-24 11:07:04 -0700

    Redux for multipage structure

--- src/lib/Blizzard.svelte
+++
@@ -1,5 +0,0 @@
-<section>
-    <h1>Nice Curves</h1>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur, possimus facere sunt tempora voluptas dignissimos, molestiae voluptatum nobis consequuntur vel eos commodi culpa est neque et. Sunt sit minima magnam!</p>
-    <div class="curve"></div>
-</section>
