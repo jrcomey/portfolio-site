c3ef2830659cc0f2f4dcab552fa405f6bfd8b684
Jack Comey <jack.rhys.comey@gmail.com>
2023-08-25 15:33:21 -0700

    Updated the 'About Me' section on the homepage

--- src/routes/ProjectGallery.svelte
+++ src/routes/ProjectGallery.svelte
@@ -1,25 +1,30 @@
 <script>
+    let black = "#0f1112";
+    let bluegrey = "#181c1f";
+    let darkgrey = "#22272b";
+    let white = "#FFFFFF";
 
 
 </script>
 
 <section id="misc-projects" class = "dark centered">
-    <h2>Project Gallery</h2>
+    <h1>Project Gallery</h1>
     
     
     <div class="project-grid">
 
 
         <div class="project">
-            <img src="./src/assets/newplot.png" alt="Project 3">
-            <h3>Rotorcraft Dynamics Simulation Package</h3>
-            <p>A package intended to reduce testing costs in vehicle development, and to provide a testing environment if one is unavailable.</p>
+                <a href="/projects/UAVsim"><img src="./src/assets/newplot.png" alt="Project 3"></a>
+                <h2>UAVSim</h2>
+                <h3>Rotorcraft Dynamics Simulation Package</h3>
+                <p>A package intended to reduce testing costs in vehicle development, and to provide a testing environment if one is unavailable.</p>            
         </div>
-
 
         <div class="project">
             <img src="./src/assets/poster_req_review.png" alt="Project 3">
-            <h3>Clementine | Space Systems Engineering</h3>
+            <h2>Clementine</h2>
+            <h3>Space Systems Engineering</h3>
             <p>
                 As part of a team of 5, I was tasked with reverse-engineering the 1993 Clementine lunar flyby probe in order to modify the spacecraft to prevent an ADCS error that caused mission failure.
             </p>    
@@ -28,7 +33,8 @@
 
         <div class="project">
             <img src="./src/assets/mozziepic.png" alt="Project 3">
-            <h3>Aerobrick | Aircraft Design</h3>
+            <h2>Aerobrick</h2>
+            <h3>Fixed-wing Aircraft Design</h3>
             <p>
                 I was Team Captain of a heavy-lift aircraft design team that competed in SAE Aero Design West's Regular division. 
             </p>    
@@ -37,27 +43,29 @@
 
         <div class="project">
             <img src="./src/assets/Undeformed.png" alt="Project 3">
+            <h2>Launch Simulation</h2>
             <h3>Satellite Inertial Loading and Thermal Analysis</h3>
             <p>Analysis of satellite superstructure experiencing inertial loading during launch, and thermal loading during operation</p>
         </div>
 
 
         <div class="project">
-            <img src="./src/assets/windy_hill_forest_1.jpg" alt="Project 4">
+            <img src="./src/assets/Prob1bResults1.png" alt="Project 4">
+            <h2>Flat Plate Drag</h2>
             <h3>CFD | Fluid Flow Analysis</h3>
             <p>Two-dimensional analysis of subsonic fluid flow over a flat plate using numerical methods</p>
         </div>
 
 
         <div class="project">
-            <img src="./src/assets/windy_hill_forest_1.jpg" alt="Project 3">
+            <img src="./src/assets/Prob1b.png" alt="Project 3">
             <h3>Wave Propagation Analysis</h3>
             <p>A three-dimensional study of wave propagation and subsonic/supersonic airflow</p>
         </div>
 
 
         <div class="project">
-            <img src="./src/assets/windy_hill_forest_1.jpg" alt="Project 4">
+            <img src="./src/assets/basinsofattraction.png" alt="Project 4">
             <h3>Basins of Attraction</h3>
             <p>Convergence study for system using imaginary numbers</p>
         </div>
@@ -65,7 +73,7 @@
 
 
         <div class="project">
-            <img src="./src/assets/windy_hill_forest_1.jpg" alt="Project 3">
+            <img src="./src/assets/Prob2bResults.png" alt="Project 3">
             <h3>Supersonic Flow</h3>
             <p>Two-dimensional analysis of supersonic flow over a symmetric biconvex airfoil</p>
         </div>
@@ -81,5 +89,48 @@
         <img class="image" src="./src/assets/windy_hill_forest_1.jpg" alt="Project 3">
         <img class="image" src="./src/assets/windy_hill_forest_1.jpg" alt="Project 3">
     </div> -->
+
+
         
 </section>
+
+<style>
+    
+.project-grid {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr); /* Adjust the number of columns as needed */
+  grid-gap: 20px;
+}
+
+.project {
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: column;
+    text-align: center;
+    margin-bottom: 30px;
+    background-color: #22272b;
+    border-radius: 1%;
+}
+
+.project img {
+  aspect-ratio: 1 / 1;
+  border-radius: 5px;
+  width: 100%;
+  object-fit: cover;
+  /* height: 50%; */
+}
+
+.project p {
+  padding: 5%;
+}
+
+.project h3 {
+    /* font-family: 'Helvetica', sans-serif; */
+    font-style: italic;
+}
+
+section {
+    background-color: #0f1112;
+}
+
+</style>
--- src/routes/ProjectHighlights.svelte
+++ src/routes/ProjectHighlights.svelte
@@ -6,22 +6,15 @@
     <h1>Project Highlights</h1>
     
     <div class="big-project-title-page">
-                
         <img src="src/assets/BlockDiagTransparent4.png" class="big-project-image-title-page" alt="MULTIROTOR GUST REDUCTION SYSTEM">
         <div class="big-project-description">
-            <h3>MULTIROTOR GUST REDUCTION SYSTEM</h3>
+            <h2>Multirotor Gust Reduction System</h2>
+            <h3>Weather Tolerant Controls Scheme for High Wind Environments</h3>
             <p>
-                As part of my undergraduate design project (see below), I worked with my team to 
-                develop a system for the Blizzard eVTOL UAM aircraft to withstand both significant 
-                gusts and other wind fields common to the Chicago area. I developed a fully fledged
-                Guidance, Navigation, and Control system (GNC), using multiple controllers, to operate 
-                the aircraft during flight. I developed optimal controllers using a Linear Quadratic 
-                Regulator (LQR) from a linearized model, and demonstrated the controller using my 
-                UAV simulation. I have currently performed dozens of tests to verify flight performance 
-                under windy conditions.
+                As part of my undergraduate design project, I worked with my team to develop a system for the Blizzard eVTOL UAM aircraft to withstand both significant gusts and other wind fields common to the Chicago area. I developed a fully fledged Guidance, Navigation, and Control system (GNC), using multiple controllers, to operate the aircraft during flight. I developed optimal controllers using a Linear Quadratic Regulator (LQR) from a linearized model, and demonstrated the controller using my UAV simulation. I performed dozens of simulation tests to verify flight performance under windy conditions.
             </p>
             <p>
-                To read more about it, click here.
+                To read more about it, <a href="/projects/grs">click here</a>.
             </p>
         </div>
     </div>
@@ -29,12 +22,13 @@
     <div class="big-project-title-page">
         <img src="src/assets/blizz.png" class="big-project-image-title-page" alt="ATP-XW BLIZZARD: WEATHER TOLERANT URBAN AIR MOBILITY CONCEPT">
         <div class="big-project-description">
-            <h3>ATP-XW BLIZZARD: WEATHER TOLERANT URBAN AIR MOBILITY CONCEPT</h3>
+            <h2>ATP-XW Blizzard</h2>
+            <h3>Weather Tolerant Urban Air Mobility Concept</h3>
             <p>
                 As part of a team of 5, my team and I are developing a weather tolerant, hydrogen powered Urban Air Mobility (UAM) vehicle, designed for operation in Chicago's cold-weather environment. I developed a fully functional Guidance, Navigation, and Control (GNC) system to enable autonomous flight for the aircraft. I also developed significant parts of the propulsion train for the aircraft, developing propellers using Blade Element Momentum (BEM) theory. I also created a flight model for the aircraft and proved the viability of the control method using a Monte Carlo simulation. I additionally designed the propeller for the aircraft, and performed center of mass calculations.
             </p>
             <p>
-                To read more about it, click here.
+                To read more about it, <a href="/projects/blizzard">click here</a>.
             </p>
         </div>
     </div>
@@ -47,19 +41,32 @@
     .big-project-title-page {
         display: grid;
         grid-template-columns: 1fr 2fr;
+        background-color: #22272b;
+        margin-top: 1%;
+        margin-bottom: 1%;
+        padding: 5% 5%;
     }
 
     .big-project-image-title-page {
         flex: 1;
-        width: 300px;
-        height: 200px;
+        width: 100%;
+        aspect-ratio: 16/9;
         object-fit: scale-down;
-        border-radius: 5px;
     }
 
     .big-project-description {
         flex: 2;
         padding: 0 20px; /* Adjust the padding as needed */
     }
+
+    .big-project-title-page h2 {
+        font-variant: small-caps;
+    }
+
+    .big-project-title-page h3 {
+        font-style: italic;
+        font-variant: small-caps;
+    }
+
     
 </style>
