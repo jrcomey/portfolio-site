6314172e09bcafd3b0ed6e8d678c461c78417f82
Jack Comey <jack.rhys.comey@gmail.com>
2023-08-26 13:26:35 -0700

    Added error page with routing back to home

---
+++ src/routes/+error.svelte
@@ -0,0 +1,29 @@
+<script>
+    import { page } from '$app/stores';
+    import Header from './Header.svelte';
+</script>
+
+<Header/>
+
+
+<section class="error">
+    <h1>{$page.status}: {$page.error.message}</h1>
+
+    <a href="/">
+        <h2>Go to homepage</h2>
+    </a>
+</section>
+
+
+
+
+<style>
+
+.error {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    align-items: center;
+}
+    
+</style>
