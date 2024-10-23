463c6d225a37375a9ba060821ed19baa1001f82f
jrcomey <65516764+jrcomey@users.noreply.github.com>
2024-10-23 15:17:02 -0700

    Update svelte.config.js for GitHub pages prep

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+    kit: {
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: '404.html',
+            precompress: false,
+            strict: true
+        }),
+        paths: {
+            // Important: replace 'your-repo-name' with your actual repository name
+            base: process.env.NODE_ENV === 'production' ? '/your-repo-name' : ''
+        }
+    }
 };
 
 export default config;
