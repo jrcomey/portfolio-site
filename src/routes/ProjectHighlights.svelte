b75edd28f0701d22bfd457b12d5b07be4764904d
Jack Comey <jack.rhys.comey@gmail.com>
2024-10-28 19:45:20 -0700

    Test path changes

--- src/routes/ProjectHighlights.svelte
+++ src/routes/ProjectHighlights.svelte
@@ -1,5 +1,6 @@
 <script>
-
+    import { base } from '$app/paths';
+    const blizz_render_path = `${base}/assets/blizz.png`
 </script>
 
 <section class="dark centered">
@@ -8,7 +9,7 @@
     <div class="big-project-title-page">
 
         <div class="project-image-backdrop-white">
-            <img src="/assets/BlockDiagTransparent4.png" class="big-project-image-title-page" alt="MULTIROTOR GUST REDUCTION SYSTEM">
+            <img src=`${base}/assets/BlockDiagTransparent4.png` class="big-project-image-title-page" alt="MULTIROTOR GUST REDUCTION SYSTEM">
         </div>
         <div class="big-project-description">
             <h1>Multirotor Gust Reduction System</h1>
@@ -17,14 +18,14 @@
                 As part of my undergraduate design project, I worked with my team to develop a system for the Blizzard eVTOL UAM aircraft to withstand both significant gusts and other wind fields common to the Chicago area. I developed a fully fledged Guidance, Navigation, and Control system (GNC), using multiple controllers, to operate the aircraft during flight. I developed optimal controllers using a Linear Quadratic Regulator (LQR) from a linearized model, and demonstrated the controller using my UAV simulation. I performed dozens of simulation tests to verify flight performance under windy conditions.
             </p>
             <p>
-                To read more about it, <a href="/projects/grs">click here</a>.
+                To read more about it, <a href=`${base}/projects/grs`>click here</a>.
             </p>
         </div>
     </div>
 
     <div class="big-project-title-page">
         <div class="project-image-backdrop-white">
-            <img src="/assets/blizz.png" class="big-project-image-title-page" alt="ATP-XW BLIZZARD: WEATHER TOLERANT URBAN AIR MOBILITY CONCEPT">
+            <img src={blizz_render_path} class="big-project-image-title-page" alt="ATP-XW BLIZZARD: WEATHER TOLERANT URBAN AIR MOBILITY CONCEPT">
         </div>
         
         <div class="big-project-description">
@@ -34,7 +35,7 @@
                 As part of a team of 5, my team and I are developing a weather tolerant, hydrogen powered Urban Air Mobility (UAM) vehicle, designed for operation in Chicago's cold-weather environment. I developed a fully functional Guidance, Navigation, and Control (GNC) system to enable autonomous flight for the aircraft. I also developed significant parts of the propulsion train for the aircraft, developing propellers using Blade Element Momentum (BEM) theory. I also created a flight model for the aircraft and proved the viability of the control method using a Monte Carlo simulation. I additionally designed the propeller for the aircraft, and performed center of mass calculations.
             </p>
             <p>
-                To read more about it, <a href="/projects/blizzard">click here</a>.
+                To read more about it, <a href=`${base}/projects/blizzard`>click here</a>.
             </p>
         </div>
     </div>
