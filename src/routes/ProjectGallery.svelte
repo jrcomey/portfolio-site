57d5e9778c1888d5ccc40bc78142bd64b5e35c42
Jack Comey <jack.rhys.comey@gmail.com>
2024-10-29 22:29:00 -0700

    Gallery image routing

--- src/routes/ProjectGallery.svelte
+++ src/routes/ProjectGallery.svelte
@@ -17,76 +17,76 @@
 
 
         <div class="project">
-            <a href="{base}/projects/UAVsim"><img src="/assets/newplot.png" alt="UAVSim Plot"></a>
-            <a href="/projects/UAVsim"><h2>UAVSim</h2></a>
-            <a href="/projects/UAVsim"><h3>Rotorcraft Dynamics Simulation Package</h3></a>
-            <a href="/projects/UAVsim"><p>A package intended to reduce testing costs in vehicle development, and to provide a testing environment if one is unavailable.</p></a>
+            <a href="{base}/projects/UAVsim"><img src="{base}/assets/newplot.png" alt="UAVSim Plot"></a>
+            <a href="{base}/projects/UAVsim"><h2>UAVSim</h2></a>
+            <a href="{base}/projects/UAVsim"><h3>Rotorcraft Dynamics Simulation Package</h3></a>
+            <a href="{base}/projects/UAVsim"><p>A package intended to reduce testing costs in vehicle development, and to provide a testing environment if one is unavailable.</p></a>
         </div>
 
         <div class="project">
-            <a href="/projects/clementine"><img src="/assets/poster_req_review.png" alt="Clementine"></a>
-            <a href="/projects/clementine"><h2>Clementine</h2></a>
-            <a href="/projects/clementine"><h3>Space Systems Engineering</h3></a>
-            <a href="/projects/clementine"><p>As part of a team of 5, I was tasked with reverse-engineering the 1993 Clementine lunar flyby probe in order to modify the spacecraft to prevent an ADCS error that caused mission failure.</p></a>    
-        </div>
-
-
-        <div class="project">
-            <a href="/projects/aerobrick"><img src="/assets/mozziepic.png" alt="Aerobrick"></a>
-            <a href="/projects/aerobrick"><h2>Aerobrick</h2></a>
-            <a href="/projects/aerobrick"><h3>Fixed-wing Aircraft Design</h3></a>
-            <a href="/projects/aerobrick"><p>I was Team Captain of a heavy-lift aircraft design team that competed in SAE Aero Design West's Regular division. </p></a>
+            <a href="{base}/projects/clementine"><img src="{base}/assets/poster_req_review.png" alt="Clementine"></a>
+            <a href="{base}/projects/clementine"><h2>Clementine</h2></a>
+            <a href="{base}/projects/clementine"><h3>Space Systems Engineering</h3></a>
+            <a href="{base}/projects/clementine"><p>As part of a team of 5, I was tasked with reverse-engineering the 1993 Clementine lunar flyby probe in order to modify the spacecraft to prevent an ADCS error that caused mission failure.</p></a>    
         </div>
 
 
         <div class="project">
-            <a href="/projects/launchsim"><img src="/assets/Undeformed.png" alt="Deformed Satellite"></a>
-            <a href="/projects/launchsim"><h2>Launch Simulation</h2></a>
-            <a href="/projects/launchsim"><h3>Satellite Inertial Loading and Thermal Analysis</h3></a>
-            <a href="/projects/launchsim"><p>Analysis of satellite superstructure experiencing inertial loading during launch, and thermal loading during operation</p></a>
+            <a href="{base}/projects/aerobrick"><img src="{base}/assets/mozziepic.png" alt="Aerobrick"></a>
+            <a href="{base}/projects/aerobrick"><h2>Aerobrick</h2></a>
+            <a href="{base}/projects/aerobrick"><h3>Fixed-wing Aircraft Design</h3></a>
+            <a href="{base}/projects/aerobrick"><p>I was Team Captain of a heavy-lift aircraft design team that competed in SAE Aero Design West's Regular division. </p></a>
         </div>
 
 
         <div class="project">
-            <a href="/projects/flat-plate-drag"><img src="/assets/Prob1bResults1.png" alt="Reynolds Flow Pic"></a>
-            <a href="/projects/flat-plate-drag"><h2>Flat Plate Drag</h2></a>
-            <a href="/projects/flat-plate-drag"><h3>CFD & Fluid Flow Analysis</h3></a>
-            <a href="/projects/flat-plate-drag"><p>Two-dimensional analysis of subsonic fluid flow over a flat plate using numerical methods</p></a>
+            <a href="{base}/projects/launchsim"><img src="{base}/assets/Undeformed.png" alt="Deformed Satellite"></a>
+            <a href="{base}/projects/launchsim"><h2>Launch Simulation</h2></a>
+            <a href="{base}/projects/launchsim"><h3>Satellite Inertial Loading and Thermal Analysis</h3></a>
+            <a href="{base}/projects/launchsim"><p>Analysis of satellite superstructure experiencing inertial loading during launch, and thermal loading during operation</p></a>
         </div>
 
 
         <div class="project">
-            <a href="/projects/waveprop"><img src="/assets/Prob1b.png" alt=""></a>
-            <a href="/projects/waveprop"><h2>Wave Propagation Analysis</h2></a>
-            <a href="/projects/waveprop"><h3>2D Discrete Differential Equation Solutions</h3></a>
-            <a href="/projects/waveprop"><p>A three-dimensional study of wave propagation and subsonic/supersonic airflow</p></a>
+            <a href="{base}/projects/flat-plate-drag"><img src="{base}/assets/Prob1bResults1.png" alt="Reynolds Flow Pic"></a>
+            <a href="{base}/projects/flat-plate-drag"><h2>Flat Plate Drag</h2></a>
+            <a href="{base}/projects/flat-plate-drag"><h3>CFD & Fluid Flow Analysis</h3></a>
+            <a href="{base}/projects/flat-plate-drag"><p>Two-dimensional analysis of subsonic fluid flow over a flat plate using numerical methods</p></a>
         </div>
 
 
         <div class="project">
-            <a href="/projects/basins"><img src="/assets/basinsofattraction.png" alt="Project 4"></a>
-            <a href="/projects/basins"><h2>Basins of Attraction</h2></a>
+            <a href="{base}/projects/waveprop"><img src="{base}/assets/Prob1b.png" alt=""></a>
+            <a href="{base}/projects/waveprop"><h2>Wave Propagation Analysis</h2></a>
+            <a href="{base}/projects/waveprop"><h3>2D Discrete Differential Equation Solutions</h3></a>
+            <a href="{base}/projects/waveprop"><p>A three-dimensional study of wave propagation and subsonic/supersonic airflow</p></a>
+        </div>
+
+
+        <div class="project">
+            <a href="{base}/projects/basins"><img src="{base}/assets/basinsofattraction.png" alt="Project 4"></a>
+            <a href="{base}/projects/basins"><h2>Basins of Attraction</h2></a>
             <div></div>
-            <a href="/projects/basins"><h3>Iterative Convergence Study</h3></a>
-            <a href="/projects/basins"><p>Fractal convergence study for system using imaginary numbers</p></a>
+            <a href="{base}/projects/basins"><h3>Iterative Convergence Study</h3></a>
+            <a href="{base}/projects/basins"><p>Fractal convergence study for system using imaginary numbers</p></a>
         </div>
 
         <div class="project">
-            <a href="/projects/supersonic"><img src="/assets/Prob2bResults.png" alt="Project 3"></a>
-            <a href="/projects/supersonic"><h3>Supersonic Flow</h3></a>
-            <a href="/projects/supersonic"><p>Two-dimensional analysis of supersonic flow over a symmetric biconvex airfoil</p></a>
+            <a href="{base}/projects/supersonic"><img src="{base}/assets/Prob2bResults.png" alt="Project 3"></a>
+            <a href="{base}/projects/supersonic"><h3>Supersonic Flow</h3></a>
+            <a href="{base}/projects/supersonic"><p>Two-dimensional analysis of supersonic flow over a symmetric biconvex airfoil</p></a>
         </div>
 
     </div>
 
     <!-- <div id="image-track" data-mouse-down-at="0">
-        <img class="image" src="/assets/windy_hill_forest_1.jpg" alt="Project 3">
-        <img class="image" src="/assets/windy_hill_forest_1.jpg" alt="Project 3">
-        <img class="image" src="/assets/windy_hill_forest_1.jpg" alt="Project 3">
-        <img class="image" src="/assets/windy_hill_forest_1.jpg" alt="Project 3">
-        <img class="image" src="/assets/windy_hill_forest_1.jpg" alt="Project 3">
-        <img class="image" src="/assets/windy_hill_forest_1.jpg" alt="Project 3">
-        <img class="image" src="/assets/windy_hill_forest_1.jpg" alt="Project 3">
+        <img class="image" src="{base}/assets/windy_hill_forest_1.jpg" alt="Project 3">
+        <img class="image" src="{base}/assets/windy_hill_forest_1.jpg" alt="Project 3">
+        <img class="image" src="{base}/assets/windy_hill_forest_1.jpg" alt="Project 3">
+        <img class="image" src="{base}/assets/windy_hill_forest_1.jpg" alt="Project 3">
+        <img class="image" src="{base}/assets/windy_hill_forest_1.jpg" alt="Project 3">
+        <img class="image" src="{base}/assets/windy_hill_forest_1.jpg" alt="Project 3">
+        <img class="image" src="{base}/assets/windy_hill_forest_1.jpg" alt="Project 3">
     </div> -->
 
 
