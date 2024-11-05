8ad59a7eea3b9460750d57a1ea0468cc16051549
Jack <jrcomey@github.com>
2024-11-05 12:21:19 -0800

    MultiVAC page

---
+++ src/routes/projects/multivac/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    /** @type {import('./$types').PageData} */
+    export let data;
+    import Header from '../../Header.svelte';
+</script>
+
+<Header/>
+
+<section>
+
+    <h1>Multivac</h1>
+
+</section>
+
+
+
+<style>
+
+</style>
